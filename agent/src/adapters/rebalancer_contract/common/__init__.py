565f2e2ec53e31ddbb0230746931270555979fc6
edsonalcala <edson_alcala@msn.com>
2025-12-18 16:29:57 +0100

    removed context

--- agent/src/adapters/rebalancer_contract/common/__init__.py
+++ agent/src/adapters/rebalancer_contract/common/__init__.py
@@ -1,9 +1,7 @@
 from .common import *
 from ._base_rebalancer import _RebalancerBase
-from .context import RebalancerContext
 
 __all__ = [
     "TGAS",
-    "_RebalancerBase",
-    "RebalancerContext",
+    "_RebalancerBase"
 ]
