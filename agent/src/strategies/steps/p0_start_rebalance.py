9150989c34147d801da1275286991740a3d7304b
edsonalcala <edson_alcala@msn.com>
2026-01-05 18:05:42 +0100

    fix start rebalance

--- agent/src/strategies/steps/p0_start_rebalance.py
+++ agent/src/strategies/steps/p0_start_rebalance.py
@@ -5,13 +5,17 @@ from .step import Step
 
 class StartRebalance(Step):
     NAME = "StartRebalance"
-
+    CAN_BE_RESTARTED = False
     async def run(self, ctx: StrategyContext) -> None:
-        print("Starting rebalance...")
+        print("🚀 Starting rebalance...")
+        if ctx.usdc_agent_balance_before_rebalance is None:
+            raise ValueError("usdc_agent_balance_before_rebalance must be set before starting rebalance.")
+        
         ctx.nonce = await ctx.rebalancer_contract.start_rebalance(
             flow=ctx.flow,
             source_chain=ctx.from_chain_id,
             destination_chain=ctx.to_chain_id,
-            expected_amount=ctx.amount
+            expected_amount=ctx.amount,
+            usdc_agent_balance_before=ctx.usdc_agent_balance_before_rebalance
         )
-        print(f"Started rebalance with nonce: {ctx.nonce}")
+        print(f"🚀 Started rebalance with nonce: {ctx.nonce}")
